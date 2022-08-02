60b4cc5c0cbf96aea3eb64c28be0360be14e917b
sergealeksis <serge-aleksis@yandex.ru>
2022-08-02 23:13:39 +0300

    DONE module 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  // т.к. необходимы ячейки соответствуют номерам 1:1, 2:2 и т.д.
+  // то можно взять i и увеличивать его каждым циклом на +1
+  for (i=0; i<table.rows.length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
-}
+  const status = table.querySelectorAll('tbody td');
+  status.forEach(
+        e => {if(e.hasAttribute('data-available')) {
+        e.dataset.available === 'true' ? 
+        e.parentElement.classList.add('available') : 
+        e.parentElement.classList.add('unavailable')
+      } 
+    })
+  const hiddenStatus = table.querySelectorAll('tbody tr');
+  hiddenStatus.forEach(
+        e => {if (!e.classList.contains('available') && !e.classList.contains('unavailable')) {
+        e.hidden = true
+      }
+    } 
+  )
+   
+  const gender = table.querySelectorAll('tbody td');
+  gender.forEach(
+        e => {if (e.innerText === 'f') {
+        e.parentElement.classList.add('female');
+  } else if (e.innerText === 'm') {
+        e.parentElement.classList.add('male');
+  }
+  })
+   
+  const age = table.querySelectorAll('tbody td');
+  age.forEach(
+        e => e.innerHTML < 18 ?  e.parentElement.style.textDecoration = 'line-through' : e)
+};
