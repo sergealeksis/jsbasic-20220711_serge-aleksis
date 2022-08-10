3e1d94b832f159e2b6641233a1f07d5f78cc38e9
sergealeksis <serge-aleksis@yandex.ru>
2022-08-10 23:39:37 +0300

    DONE module 4 & module 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let toMove = document.querySelector('.carousel__inner');
+  let slides = document.querySelectorAll('.carousel__slide');
+  let position = 0;
+  arrowLeft.style.display = 'none';
+  
+  arrowRight.onclick = function() {
+    if (position === toMove.offsetWidth * (slides.length - 2)) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = '';
+    };
+    arrowLeft.style.display = '';
+
+   return toMove.style.transform = `translateX(${-(position += toMove.offsetWidth)}px)`;
+  };
+
+  arrowLeft.onclick = function() {
+    if (position === toMove.offsetWidth) {
+      arrowLeft.style.display = 'none';
+    } else {
+      arrowLeft.style.display = ''
+    };
+      arrowRight.style.display = '';
+
+    return toMove.style.transform = `translateX(${-(position -= toMove.offsetWidth)}px)`;
+  };
 }
+
+  
